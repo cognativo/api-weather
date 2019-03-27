5822f3b0df79d1fda725eef408b04f2d064a41b8
TheDataResearch <drocha21@gmail.com>
2019-03-27 00:20:17 -0300

    Novo teste

--- routes/proxyOpenWeather.js
+++ routes/proxyOpenWeather.js
@@ -2,6 +2,7 @@ var axios = require('axios')
 var express = require('express');
 var router = express.Router();
 
+//Comentando para salvar novamente.
 
 //axios.get('https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1')
 router.get('/clima', function(req, response, next) {
