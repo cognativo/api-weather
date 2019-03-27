2a501346198c85067233d5a29c9f891dd5c562d5
TheDataResearch <drocha21@gmail.com>
2019-03-27 00:03:56 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var weatherRouter = require('./routes/proxyOpenWeather')
+
+var app = express();
+
+const port = 3000
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api', weatherRouter)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+module.exports = app;
---
+++ routes/proxyOpenWeather.js
@@ -0,0 +1,14 @@
+var axios = require('axios')
+var express = require('express');
+var router = express.Router();
+
+
+//axios.get('https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1')
+router.get('/clima', function(req, response, next) {
+    axios.get('https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1')
+    .then ( function(res){
+        response.send(JSON.stringify(res.data))  
+    })
+});
+
+module.exports = router
