cc3ee54c6172fde77c64c808387f8835e0bffe62
Erick da Rocha <erick.rocha@roit.com.br>
2019-03-27 08:47:28 -0300

    Alteração realizada por Erick Rocha

--- routes/proxyOpenWeather.js
+++ routes/proxyOpenWeather.js
@@ -7,8 +7,11 @@ var router = express.Router();
 //axios.get('https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1')
 router.get('/clima', function(req, response, next) {
     axios.get('https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1')
-    .then ( function(res){
+    .then (function(res){
         response.send(JSON.stringify(res.data))  
+    })
+    .catch(function(error){
+        console.error(error)
     })
 });
 
